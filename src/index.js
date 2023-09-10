b44c88680bbde4c9d6efc71658e044da04ebab14
Josefa Fontecilla <fontecillajosefa10@gmail.com>
2023-09-10 16:20:58 -0300

    proyecto listo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
-import analyzer from './analyzer.js';
+import analyzer from "./analyzer.js";
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+const textareas = document.getElementsByName("user-input");
+const userInput = textareas[0];
+userInput.addEventListener("keyup", function () {
+  const text = userInput.value;
+
+  const caracteresSinEspacioSinPuntos = document.querySelector(
+    'li[data-testid="character-no-spaces-count"]'
+  );
+  caracteresSinEspacioSinPuntos.textContent =
+    analyzer.getCharacterCountExcludingSpaces(text);
+
+  const recuentoCaracteres = document.querySelector(
+    'li[data-testid="character-count"]'
+  );
+  recuentoCaracteres.textContent = analyzer.getCharacterCount(text);
+
+  const recuentoPalabras = document.querySelector(
+    'li[data-testid="word-count"]'
+  );
+  recuentoPalabras.textContent = analyzer.getWordCount(text);
+
+  const soloNumeros = document.querySelector('li[data-testid="number-count"]');
+  soloNumeros.textContent = analyzer.getNumberCount(text);
+
+  const promedioPalabras = document.querySelector(
+    'li[data-testid="word-length-average"]'
+  );
+  promedioPalabras.textContent = analyzer.getAverageWordLength(text);
+
+  const sumaNumeros = document.querySelector('li[data-testid="number-sum"]');
+  sumaNumeros.textContent = analyzer.getNumberSum(text);
+});
+document.getElementById("reset-button").addEventListener("click", function () {
+  const métricas = document.querySelectorAll(".lista");
+  métricas.forEach(function (elementosLista) {
+    elementosLista.textContent = "0";
+
+    const textareas = document.getElementsByName("user-input");
+    textareas.forEach(function (textareas) {
+      textareas.value = "";
+    });
+  });
+});
